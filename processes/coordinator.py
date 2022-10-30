1e996ee81c567000df159a130dd73d56c1034aa9
Gagan G R <gagan2001j@gmail.com>
2022-10-30 18:29:15 +0530

    initial commit

---
+++ processes/coordinator.py
@@ -0,0 +1,52 @@
+from typing import List, Tuple
+import logging
+import argparse
+from concurrent import futures
+import grpc
+
+from generated import spec_pb2, spec_pb2_grpc
+
+
+class Coordinator(spec_pb2_grpc.Coordinator):
+    def __init__(self):
+        super(Coordinator, self).__init__()
+        self.clients: List[Tuple[int, spec_pb2_grpc.NodeStub]] = []
+
+    def Register(self, request: spec_pb2.RegisterRequest, context) -> spec_pb2.RegisterReply:
+        if self.clients:
+            reply = spec_pb2.RegisterReply(parentPort=self.clients[-1][0])
+        else:
+            reply = spec_pb2.RegisterReply(parentPort=-1)
+
+        channel = grpc.insecure_channel(f'localhost:{request.port}')
+        self.clients.append((request.port, spec_pb2_grpc.NodeStub(channel)))
+
+        print("Node Added")
+        return reply
+
+    def Write(self, request: spec_pb2.WriteRequest, context) -> spec_pb2.WriteReply:
+        while self.clients:
+            res = self.clients[0][1].Write(request)
+            if res.success:
+                return res
+            else:
+                self.clients = self.clients[1:]
+        return spec_pb2.WriteReply(success=False)
+
+
+def serve(args):
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=5))
+    spec_pb2_grpc.add_CoordinatorServicer_to_server(Coordinator(), server)
+    server.add_insecure_port(f'[::]:{args.port}')
+    server.start()
+    print("Co-Ordinator is running")
+    server.wait_for_termination()
+
+
+if __name__ == '__main__':
+    logging.basicConfig()
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-p", "--port", help="Port to run the coordinator process on.", default=5200)
+    args = parser.parse_args()
+    serve(args)
---
+++ processes/node.py
@@ -0,0 +1,53 @@
+import logging
+import argparse
+from concurrent import futures
+import grpc
+import dbm
+
+from generated import spec_pb2, spec_pb2_grpc
+
+
+class Node(spec_pb2_grpc.Node):
+
+    def __init__(self, args):
+        super(Node, self).__init__()
+        # establish connection with coordinator and get chain info.
+        # we will assume we are the tail
+        channel = grpc.insecure_channel(f'localhost:{args.coordinator}')
+        self.coordinator = spec_pb2_grpc.CoordinatorStub(channel)
+        self.args = args
+        self.parentInfo: spec_pb2.RegisterReply = self.coordinator.Register(
+            spec_pb2.RegisterRequest(port=int(args.port)))
+
+    def Read(self, request: spec_pb2.ReadRequest, context) -> spec_pb2.ReadReply:
+        with dbm.open(f'.{self.args.port}-dbm', 'r') as store:
+            val = store.get(request.key, bytearray()).decode('utf8')
+        return spec_pb2.ReadReply(key=request.key, value=val)
+
+    def Write(self, request: spec_pb2.WriteRequest, context) -> spec_pb2.WriteReply:
+        try:
+            with dbm.open(f'.{self.args.port}-dbm', 'c') as store:
+                store[request.key] = request.value
+            return spec_pb2.WriteReply(success=True)
+        except Exception:
+            return spec_pb2.WriteReply(success=False)
+
+
+def serve(args):
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=5))
+    spec_pb2_grpc.add_NodeServicer_to_server(Node(args), server)
+    server.add_insecure_port(f'[::]:{int(args.port)}')
+    server.start()
+    print("Node Started...")
+    server.wait_for_termination()
+
+
+if __name__ == '__main__':
+    logging.basicConfig()
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-p", "--port", help="Port to run the node process on.", default=5201)
+    parser.add_argument("-c", "--coordinator",
+                        help="Port of the coordinator process.", default=5200)
+    args = parser.parse_args()
+    serve(args)
